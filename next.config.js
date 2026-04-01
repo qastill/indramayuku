59853c48b2fa1406a065c1f5278d5f71d73f7079
qastill <qastill@gmail.com>
2026-04-01 15:43:32 +0700

    Add ui-avatars.com to remote patterns

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
       { protocol: 'https', hostname: '*.supabase.co' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'http', hostname: 'localhost' },
+      { protocol: 'https', hostname: 'ui-avatars.com' },
     ],
   },
 }
